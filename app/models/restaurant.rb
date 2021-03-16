7029078dada97265ac29a3f5681b1aa15e4749e1
Leonardo Marques Pereira das Neves <ledomarques@gmail.com>
2021-03-16 12:09:42 -0300

    add Order Controller

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,8 @@
 class Restaurant < ApplicationRecord
-    has_many :products
-    belongs_to :user
+  has_many :products
+  belongs_to :user
 
-    def total_of_products 
-        products.count 
-    end
+  def total_of_products 
+    products.count 
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_15_192342) do
+ActiveRecord::Schema.define(version: 2021_03_16_114625) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,14 @@ ActiveRecord::Schema.define(version: 2021_03_15_192342) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_installs_on_email", unique: true
     t.index ["reset_password_token"], name: "index_installs_on_reset_password_token", unique: true
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "product_id"
+    t.integer "status"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "products", force: :cascade do |t|
